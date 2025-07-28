4aace787047ab629dc74566c8b361bcab8846d88
Luiz <luiz.barbosa@arcoeducacao.com.br>
2025-07-28 13:27:39 -0300

    Fix: Corrige base path e refaz o build

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // A linha abaixo é a mais importante de todas
+  // Esta linha é a solução para o erro 404
   base: '/Acervo-Arco-Digital/', 
 })
