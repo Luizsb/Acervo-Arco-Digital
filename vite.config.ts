08786ba297a2b37a010c9ca304fcf2e50a523e3e
Luiz <luiz.barbosa@arcoeducacao.com.br>
2025-07-28 13:25:03 -0300

    Corrigindo o caminho base para o deploy

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Use esta linha com o nome do seu repositório
-  base: '/Acervo-Arco-Digital/',
+  // A linha abaixo é a mais importante de todas
+  base: '/Acervo-Arco-Digital/', 
 })
